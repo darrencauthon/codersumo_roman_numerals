20904d44f41bf04bc1222da0da20e5b14e017078
Darren Cauthon <darren@cauthon.com>
2013-04-15 12:36:07 -0500

    Create a method for returning the value of a letter.

--- app.rb
+++ app.rb
@@ -1,13 +1,25 @@
 module RomanNumeral
+
+  def self.value_for letter
+    case letter
+    when 'I'
+      1
+    when 'V'
+      5
+    when 'X'
+      10
+    end
+  end
+
   def self.convert_to_int input
     result = 0
     if input.include? 'XL'
       result += 40
     elsif input.include? 'X'
-      result += 10 * input.split('').select { |x| x == 'X' }.count
+      result += value_for('X') * input.split('').select { |x| x == 'X' }.count
     end
     if input.include? 'V'
-      result += 5
+      result += value_for 'V'
     end
     result += count_it input, 'I'
     result
@@ -17,9 +29,9 @@ module RomanNumeral
     return 0 unless input.include? 'I'
     if input.include? 'I'
       if input[-1] == 'I'
-        input.split('').select { |x| x == 'I' }.count
+        value_for(letter) * input.split('').select { |x| x == 'I' }.count
       else
-        -1
+        -1 * value_for(letter)
       end
     end
   end
