3873a1f47f082a7c040264f9338a70523f90c04a
Darren Cauthon <darren@cauthon.com>
2013-04-15 10:42:11 -0500

    Start on the solution.

---
+++ app.rb
@@ -0,0 +1,5 @@
+module RomanNumeral
+  def self.convert_to_int input
+    input.length
+  end
+end
---
+++ test.rb
@@ -0,0 +1,21 @@
+require_relative 'app'
+require 'minitest/spec'
+require 'minitest/autorun'
+
+def result_for input
+  RomanNumeral.convert_to_int(input)
+end
+
+describe RomanNumeral do
+  it "should return 1 for I" do
+    result_for('I').must_equal 1
+  end
+
+  it "should return 2 for II" do
+    result_for('II').must_equal 2
+  end
+
+  it "should return 3 for III" do
+    result_for('III').must_equal 3
+  end
+end
